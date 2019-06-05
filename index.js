9e0ffc68fd89892598a733578f69c9d28117387a
eager-strategy-8796 <cheniasli@gmail.com>
2019-06-05 16:03:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments) {
   var emptyArr = [];
   for (var i = 0; i < musicians.length; i++) {
-  emptyArr.push("musicians[i] + ' plays ' + instruments[i]");
+  emptyArr.push(musicians[i] + ' plays ' + instruments[i]);
 }}
 
