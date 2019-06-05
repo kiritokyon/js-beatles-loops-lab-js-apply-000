043ecafaab31f082ffbed5d290bd974c28e86cc9
eager-strategy-8796 <cheniasli@gmail.com>
2019-06-05 16:03:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function theBeatlesPlay(musicians, instruments) {
   var emptyArr = [];
   for (var i = 0; i < musicians.length; i++) {
   emptyArr.push(musicians[i] + ' plays ' + instruments[i]);
-  return emptyArr
+  return emptyArr;
 }}
 
