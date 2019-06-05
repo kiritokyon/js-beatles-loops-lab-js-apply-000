1a58440d1e2d06f34a4729724fea2521ce7a1dc9
eager-strategy-8796 <cheniasli@gmail.com>
2019-06-05 15:35:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 
+var instruments = ["guitar", ]
 var arr[];
 
 for (i = 0; i < arr.length; i++) {
