9f7fbcc8966ecc1993b58f7456c7e0e608b6ef3d
eager-strategy-8796 <cheniasli@gmail.com>
2019-06-05 16:08:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i++) {
   emptyArr.push(musicians[i] + ' plays ' + instruments[i]);
   return emptyArr;
-}}
+}
+}
 
